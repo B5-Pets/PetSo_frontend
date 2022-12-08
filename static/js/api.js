6632c5d5144358f9d985a77c015d7bf58ab4208e
som2145 <som2145@gmail.com>
2022-12-08 15:55:50 +0900

    Add post_create.html and js

--- static/js/api.js
+++ static/js/api.js
@@ -3,7 +3,6 @@
 // const backend_base_url = "http://ec2인스턴스ip주소";
 // 백엔드 서버 연결 시
 
-
 const backend_base_url = "http://127.0.0.1:8000";
 const frontend_base_url = "http://127.0.0.1:5500/templates";
 
