5dfbc4468047c96430940aebf8f1795038920a2c
JANG HYE RIM <fla12181@gmail.com>
2022-12-11 16:03:22 +0900

    Merge

--- static/js/articledetail.js
+++ static/js/articledetail.js
@@ -25,6 +25,15 @@ async function loadArticle(article_id) {
   const likes = document.getElementById("likes");
   const bookmarks = document.getElementById("bookmarks");
   const created_at = document.getElementById("created_at");
+
+  const dolike = document.getElementById("dolike");
+  const dolike_button = document.createElement("button");
+  dolike_button.innerText = "❤️";
+  dolike_button.setAttribute("id", article_id);
+  dolike_button.setAttribute("class", "btn btn-outline-danger");
+  dolike_button.setAttribute("onclick", "DoLike(this.id)");
+  console.log(dolike_button)
+  dolike.appendChild(dolike_button);
   
   title.innerText = article.title;
   user.innerText = article.user;
@@ -38,27 +47,28 @@ loadArticle(article_id);
 
 
 //Like
-$(function(){
-    var $likeBtn =$('.icon.heart');
+// $(function(){
+//     var $likeBtn =$('.icon.heart');
 
-        $likeBtn.click(function(){
-        $likeBtn.toggleClass('active');
+//         $likeBtn.click(function(){
+//         $likeBtn.toggleClass('active');
 
-        if($likeBtn.hasClass('active')){          
-           $(this).find('img').attr({
-              'src': 'https://cdn-icons-png.flaticon.com/512/803/803087.png',
-               alt:'좋아요'
-                });
+//         if($likeBtn.hasClass('active')){          
+//            $(this).find('img').attr({
+//               'src': 'https://cdn-icons-png.flaticon.com/512/803/803087.png',
+//                alt:'좋아요'
+//                 });
           
           
-         }else{
-            $(this).find('i').removeClass('fas').addClass('far')
-           $(this).find('img').attr({
-              'src': 'https://cdn-icons-png.flaticon.com/512/812/812327.png',
-              alt:"좋아요 취소"
-           })
-         }
-     })
-})
+//          }else{
+//             $(this).find('i').removeClass('fas').addClass('far')
+//            $(this).find('img').attr({
+//               'src': 'https://cdn-icons-png.flaticon.com/512/812/812327.png',
+//               alt:"좋아요 취소"
+//            })
+//          }
+//      })
+// })
+
 
 
