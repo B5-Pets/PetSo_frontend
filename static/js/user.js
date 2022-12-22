9ecdbe0d3c494b56553ffa93e6abf3c2822b1d43
MoonGyuBin <2019captain@gmail.com>
2022-12-22 14:15:09 +0900

    change bakc,front adress

--- static/js/user.js
+++ static/js/user.js
@@ -74,7 +74,7 @@ async function handleLogin() {
 async function handlegoogle() {
   let code = new URL(window.location.href).searchParams.get('code')
   if (code) {
-  const response = await fetch(`http://127.0.0.1:8000/user/google/callback/?code=${code}`)
+  const response = await fetch(`http://13.125.224.113/user/google/callback/?code=${code}`)
   const response_json = await response.json()
 
   localStorage.setItem('access', response_json.access_token);
