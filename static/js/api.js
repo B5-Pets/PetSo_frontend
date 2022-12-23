0826c35ff4fc2b263398a2127c25160e61931d01
야언 <112169271+didjsrne@users.noreply.github.com>
2022-12-23 18:22:59 +0900

    fix : 프론트, 백엔드 주소 배포환경으로 변경

--- static/js/api.js
+++ static/js/api.js
@@ -2,10 +2,10 @@
 // EC2 인스턴스 연결 시
 // const backend_base_url = "http://ec2인스턴스ip주소";
 // 백엔드 서버 연결 시
-const backend_base_url = "http://127.0.0.1:8000";
-const frontend_base_url = "http://127.0.0.1:5500";
-// const backend_base_url = "https://api.pet-so.net/";
-// const frontend_base_url = "https://pet-so.net/";
+// const backend_base_url = "http://127.0.0.1:8000";
+// const frontend_base_url = "http://127.0.0.1:5500";
+const backend_base_url = "https://api.pet-so.net/";
+const frontend_base_url = "https://pet-so.net/";
 
 
 const token = localStorage.getItem("access");
